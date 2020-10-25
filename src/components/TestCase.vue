2fd09cb65c48078022a04d214df9d982fee30b18
seveniruby <seveniruby@gmail.com>
2020-10-25 15:04:27 +0800

    测试用例真实数据绑定

--- src/components/TestCase.vue
+++ src/components/TestCase.vue
@@ -153,22 +153,18 @@
 
 
 <script>
+import http from "@/http";
+
   export default {
       name: 'TestCase',
     data: () => ({
       dialog: false,
       dialogDelete: false,
       headers: [
-        {
-          text: 'Dessert (100g serving)',
-          align: 'start',
-          sortable: false,
-          value: 'name',
-        },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
+        { text: 'id', value: 'id' },
+        { text: 'name', sortable: false, value: 'name' },
+        { text: 'description', sortable: false, value: 'description' },
+        { text: 'data', sortable: false, value: 'data' },
         { text: 'Actions', value: 'actions', sortable: false },
       ],
       desserts: [],
@@ -210,78 +206,10 @@
 
     methods: {
       initialize () {
-        this.desserts = [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-          },
-        ]
+          http.get('/testcase').then( response => {
+              console.log(response)
+              this.desserts=response.data
+          })        
       },
 
       editItem (item) {
